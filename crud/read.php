81326a75b6c9531e6e993237a5d323e9a93574f9
Arvydas <arvydas.simbelis@gmail.com>
2023-12-27 14:31:53 +0200

    crud initial commit

---
+++ crud/read.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
+    <title>Read</title>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-2">
+                Box ID
+            </div>
+            <div class="col-2">
+                Amount
+            </div>
+            <div class="col-8">
+                Action
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
